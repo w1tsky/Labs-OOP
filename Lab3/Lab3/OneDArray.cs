e4ea476f948a347708ea51a36ac5d86592821889
w1tsky <neill.white220@gmail.com>
2018-10-11 00:21:27 +0300

    Вариант 12
    Создать класс - Одномерный массив целых чисел
    (вектор). Создать индексатор, методы поэлементного
    сложения и вычитания со скалярным значением.
    Создать массив объектов. Вывести:
    a) массивы только с четными/нечетными элементами;
    b) массив с наибольшей суммой элементов

---
+++ Lab3/Lab3/OneDArray.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class OneDArray
+    {
+        // Поля
+        int[] IntArray;
+        int size = 0;
+
+        // Конструктор, позволяющий создать массив размерности n.
+        public OneDArray(int size)
+        {
+            this.size = size;
+            IntArray = new int[size];
+        }
+
+        // Метод, позволяющий ввести элементы массива с клавиатуры.
+        public void InputIntArray()
+        {
+            Console.WriteLine("Введите элименты массива: ");
+            for (int i = 0; i < IntArray.Length; i++)
+            {
+                Console.Write("A[{0}] = ", i);
+                IntArray[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        // Метод, позволяющий вывести элементы массива на экран.
+        public void ShowIntArray()
+        {
+            foreach (int item in IntArray)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+        }
+
+        // Метод, позволяющий отсортировать элементы массива в порядке возрастания.
+        public void SortingArray() => Array.Sort(IntArray);
+
+        // Свойство возвращающее размерность массива (доступное только для чтения).
+        public string Length => $"\nРазмерность массива составляет {IntArray.Length} элемента.";
+
+        // Свойство позволяющее домножить все элементы массива на скаляр (доступное только для записи).
+        public int Scalar
+        {
+            set
+            {
+                for (int i = 0; i < IntArray.Length; i++)
+                {
+                    IntArray[i] *= value;
+                }
+            }
+        }
+
+        // Индексатор, позволяющий по индексу обращаться к соответствующему элементу массива.
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < IntArray.Length)
+                {
+                    return $"Индексу [{index}] соответствует элемент {IntArray[index]}.";
+                }
+                else
+                {
+                    return $"Индекс [{index}] является недопустимым.";
+                }
+            }
+        }
+
+        // Перегрузка операции ++: одновременно увеличивает значение всех элементов массива на 1.
+        public static OneDArray operator ++(OneDArray increment)
+        {
+            OneDArray array = new OneDArray(increment.IntArray.Length);
+            for (int i = 0; i < increment.IntArray.Length; i++)
+            {
+                increment.IntArray[i] += 1;
+            }
+            return increment;
+        }
+
+        // Перегрузка операции --: одновременно уменьшает значение всех элементов массива на 1.
+        public static OneDArray operator --(OneDArray decrement)
+        {
+            OneDArray array = new OneDArray(decrement.IntArray.Length);
+            for (int i = 0; i < decrement.IntArray.Length; i++)
+            {
+                decrement.IntArray[i] -= 1;
+            }
+            return decrement;
+        }
+
+        // Перегрузка операции !: возвращает значение true, если элементы массива не упорядочены по возрастанию, иначе false.
+        public static bool operator !(OneDArray array)
+        {
+            bool sort = false;
+            for (int i = 0; i < array.IntArray.Length - 1; i++)
+                if (array.IntArray[i] > array.IntArray[i + 1])
+                {
+                    sort = true;
+                }
+            return sort;
+        }
+
+        // Перегрузка операции бинарный *: домножить все элементы массива на скаляр.
+        public static OneDArray operator *(OneDArray multiplication, int scalar)
+        {
+            OneDArray array = new OneDArray(multiplication.IntArray.Length);
+            for (int i = 0; i < multiplication.IntArray.Length; i++)
+            {
+                multiplication.IntArray[i] *= scalar;
+            }
+            return multiplication;
+        }
+
+        // Перегрузка операции преобразования класса массив в одномерный массив(и наоборот).
+        public static explicit operator OneDArray(int[] array)
+        {
+            OneDArray transformation = new OneDArray(array.Length);
+            array.CopyTo(transformation.IntArray, 0);
+            return transformation;
+        }
+
+        public static explicit operator int[] (OneDArray transformation)
+        {
+            return transformation.IntArray;
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Задайте размер массива: ");
+            int size = int.Parse(Console.ReadLine());
+
+            OneDArray array = new OneDArray(size);   // Инициализация одномерного массива
+            array.InputIntArray();                   // Ввод элементов массива с клавиатуры
+            Console.WriteLine("\nИсходный массив:");
+            array.ShowIntArray();                    // Вывод на экран исходных элементов массива
+
+            array.SortingArray();                    // Сортировка элементов массива в порядке возростания
+            Console.WriteLine("\nОтсортированный в порядке возростания массив:");
+            array.ShowIntArray();                    // Вывод на экран отсортированных элементов массива
+
+            Console.WriteLine(array.Length);         // Информация о размерности массива
+            Console.WriteLine("\nМассив, элементы которого умножены на скаляр (3):");
+            array.Scalar = 3;
+            array.ShowIntArray();                    // Элементы массива умноженные на скаляр
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\nОбращение к элементам массива по индексу:");
+            Console.ResetColor();
+            Console.WriteLine(array[-1]);            // Обращение к элементам массива по индексу
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[2]);
+            Console.WriteLine(array[7]);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\nДемонстрация перегрузки операций");
+            Console.ResetColor();
+            Console.Write("Перегрузка операции ++: ");
+            ++array;                                 // Перегрузка операции ++
+            array.ShowIntArray();
+            Console.Write("Перегрузка операции --: ");
+            --array;                                 // Перегрузка операции --
+            array.ShowIntArray();
+            Console.Write("Перегрузка операции *5: ");
+            array *= 5;                              // Перегрузка операции *
+            array.ShowIntArray();
+
+            if (!array)                              // Перегрузка операции !
+            {
+                Console.WriteLine("Элементы массива НЕ упорядочены по возрастанию!");
+            }
+            else
+            {
+                Console.WriteLine("Элементы массива упорядочены по возрастанию.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
